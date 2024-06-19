772052102e55c8fff8056fc4aa67cce4eb32b09e
iicorleone <emugimba@gmail.com>
2024-06-19 19:07:29 +0000

    Updates write to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ category = st.selectbox('Select a vehicle:', dfb['vehicle'].unique())
 filtered_data = dfb[dfb['vehicle'] == category]
 st.write("Results")
 # Display filtered data
-st.write(filtered_data['start_address'].value_counts())
+st.dataframe(filtered_data['start_address'].value_counts())
 
 st.dataframe(dft, column_config={
         "vehicle": "Vehicle",
