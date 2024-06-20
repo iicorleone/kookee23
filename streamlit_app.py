369acf87ac89c5287ced69238e6ee37d7dcffd40
iicorleone <emugimba@gmail.com>
2024-06-20 08:12:49 +0000

    Date Filter default dates

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ st.dataframe(counts)
 #Sidebar
 with st.sidebar:
 #Date Range Filter
-    start_date = st.date_input('Start date', datetime.today())
-    end_date = st.date_input('End date', datetime.today())
+    start_date = st.date_input('Start date', datetime.date(2023,06,01))
+    end_date = st.date_input('End date', datetime.date(2024,02,29))
 
     # Dropdown for filtering Vehicle
     category = st.selectbox('Select a vehicle:', dfb['vehicle'].unique())
